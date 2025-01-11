e7d80568fe34db6bed2051332dbba1b89d5d654f
mohit5tech <mohit.kumar@5techlab.com>
2025-01-11 19:53:42 +0530

    Updated configuration
    Updated configuration

--- scripts/layout_detection.py
+++ scripts/layout_detection.py
@@ -27,11 +27,11 @@ def main(config_path):
     model_layout_detection = task_instances[TASK_NAME]
 
     # for image detection
-    detection_results = model_layout_detection.predict_images(input_data, result_path)
+    #detection_results = model_layout_detection.predict_images(input_data, result_path)
 
     # for pdf detection
-    # detection_results = model_layout_detection.predict_pdfs(input_data, result_path)
-
+    detection_results = model_layout_detection.predict_pdfs(input_data, result_path)
+    print("upto when")
     # print(detection_results)
     print(f'The predicted results can be found at {result_path}')
 
